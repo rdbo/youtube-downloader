5e837b980f1c93d7e941a2be0a012a9e767c8a7c
rdbo <rdbodev@gmail.com>
2025-01-28 09:51:37 +0000

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useRef } from 'react';
+
 export default function App() {
+  const videoUrlRef = useRef<HTMLInputElement>(null);
+  const videoInfoRef = useRef<HTMLSpanElement>(null);
+
+  const getVideoInfo = async () => {
+    console.log("Getting video");
+    videoInfoRef.current.innerText = "TEST";
+  };
+
   return (
-    <main className="container">
+    <main>
       <h1>Test</h1>
+      <label>URL: </label>
+      <input ref={videoUrlRef}/>
+      <button onClick={getVideoInfo}>Get Info</button>
+      <span ref={videoInfoRef}></span>
     </main>
   );
 }
